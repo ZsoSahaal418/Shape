e8ab77e6dac10bdf493d0c4170935e2bb66e4479
ZsoSahaal418 <jhammer15.dixon@gmail.com>
2016-12-16 16:02:44 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shape;
 
-/**
- *
- * @author John and Cass
- */
 public class Rectangle implements Shape {
     
     double width;
     double height;
     
+    //creates a rectangle with width w and height h.
     Rectangle(double w, double h)
     {
         this.width = w;
