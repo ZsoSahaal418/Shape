c319707f2e2fe4cc5cb774a24c4212d47974e3fa
ZsoSahaal418 <jhammer15.dixon@gmail.com>
2016-12-16 16:05:10 -0500

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shape;
 
-/**
- *
- * @author John and Cass
- */
+//interface as given for excersize
 public interface Shape {
     public double getArea();
     public double getPerimeter();
